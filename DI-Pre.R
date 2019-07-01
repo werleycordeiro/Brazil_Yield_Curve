9ce6640712207903b636505371c0637c93ac4d15
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-01 16:33:19 -0300

    Update DI-Pre.R

--- DI-Pre.R
+++ DI-Pre.R
@@ -1,1 +1,13 @@
+install.packages(c("rvest","httr"))
 
+library(httr)
+library(rvest)
+
+data = "06/28/2019" # First date available: "01/02/2004"
+di = GET(url = "http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-taxas-referenciais-bmf-enUS.asp",query = list(Data = data))
+di = read_html(di) %>% html_nodes("table")
+data = di %>% html_nodes("td") %>% html_text()
+data = data.frame(matrix(data, ncol=3, byrow=TRUE))
+colnames(data) = c("Calendar Days","252 Days","360 Days")
+head(data)
+ts.plot(data[,2])
