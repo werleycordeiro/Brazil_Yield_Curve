f0b9833e42e67e4a998db57e44a9f5c044ee1470
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-01 23:34:37 -0300

    Update DI-Pre.R

--- DI-Pre.R
+++ DI-Pre.R
@@ -7,11 +7,13 @@ library(httr)
 library(rvest)
 
 # Scraping
-data = "06/28/2019" # First data available: "01/02/2004"
-di = GET(url = "http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-taxas-referenciais-bmf-enUS.asp",query = list(Data = data))
+data = "05/10/2018" # First data available: "01/02/2004"
+di = GET(url = "http://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-taxas-referenciais-bmf-ptBR.asp",query = list(Data = data))
 di = read_html(di) %>% html_nodes("table")
 data = di %>% html_nodes("td") %>% html_text()
 data = data.frame(matrix(data, ncol=3, byrow=TRUE))
+data[,2] = as.numeric(gsub(",", ".", gsub("\\.", "", data[,2])))
+data[,3] = as.numeric(gsub(",", ".", gsub("\\.", "", data[,3])))
 colnames(data) = c("Calendar Days","252 Days","360 Days")
 head(data)
 ts.plot(data[,2])
@@ -24,3 +26,5 @@ t.new <- c(1,3,6,9,12,15,18,21,24,27,30,33,36,39,48,60,72) # Maturities
 new<-predict(spl, t.new)
 plot(new, ylab = 'Yield', xlab = 'months', main = 'Yield Curve')
 lines(new)
+
+format(seq(as.Date('2019/01/02'), as.Date('2019/06/28'), 'day'), format="%d-%m-%Y", tz="UTC")
